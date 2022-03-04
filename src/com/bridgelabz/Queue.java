b8dfad174c3a6ab27f94e959c6f29f25c250c8a3
tomthomas1 <99910123+tomthomas1@users.noreply.github.com>
2022-03-04 19:32:33 +0530

    UC3

---
+++ src/com/bridgelabz/Queue.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+/**
+ * Main method for the Queue
+ * We have called the methods in the main class
+ * @author Tom
+ *
+ */
+public class Queue {
+	public static void main(String[] args) {
+
+		/**
+		 * Created a object of service class and called the method
+		 */
+		QueueServices queue = new QueueServices();
+		queue.enqueu(56);
+		queue.enqueu(30);
+		queue.enqueu(70);
+
+		queue.display();
+
+	}
+}
